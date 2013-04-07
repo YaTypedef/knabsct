fa8aa736ac5c50b823f1fce9596b09157134b9d5
Dmitry Krylov <mitya.krylov@gmail.com>
2013-04-08 02:07:36 +0400

    print result stats as one file for each merchant for easier import to excel

--- merchants/merchants/main.cpp
+++ merchants/merchants/main.cpp
@@ -4,18 +4,19 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <sstream>
 
 using namespace NMerchants;
 
 int main(int argc, const char** argv) {
     if (argc != 4) {
-        std::cout << "Usage: merchants <path_to_transactions> <path_to_mcc_codes> <output_file>" << std::endl;
+        std::cout << "Usage: merchants <path_to_transactions> <path_to_mcc_codes> <output_file_prefix>" << std::endl;
         return 0;
     }
 
     const std::string pathTransactions = argv[1];
     const std::string pathMccCodes = argv[2];
-    const std::string pathOutput = argv[3];
+    const std::string pathOutputPrefix = argv[3];
 
     TTransactions transactions = ReadTransactionsFromFile(pathTransactions);
     TMccDescriptionTable mccDescriptionTable = ReadMccDescriptionTableFromFile(pathMccCodes);
@@ -34,9 +35,12 @@ int main(int argc, const char** argv) {
         stats[merchantIndex].AddRecord(*it);
     }
 
-    std::ofstream output(pathOutput);
+    
 
     for (size_t merchantIndex = FIRST_MERCHANT_INDEX; merchantIndex < stats.size(); ++merchantIndex) {
+        std::ostringstream pathOutputOss;
+        pathOutputOss << pathOutputPrefix << "_" << merchantIndex << ".txt";
+        std::ofstream output(pathOutputOss.str());
         output << "MerchantId" << "\t" << merchantIndex << std::endl;
         stats[merchantIndex].PrintToStream(output);
     }
